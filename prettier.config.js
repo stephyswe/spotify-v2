0f9c4d26b0dba23d2b445ba447d7ba2ecead7e19
Stephanie <stephaniiehallberg@gmail.com>
2023-06-07 09:03:21 +0200

    feat(app): Environment setup

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  importOrder: [
+    "^@core/(.*)$",
+    "<THIRD_PARTY_MODULES>",
+    "^@/(.*)$",
+    "^@/app/(.*)$",
+    "^@/components/(.*)$",
+    "^@/libs/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
